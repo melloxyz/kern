91028399c705bb7facb24473e8b2cb9b110f5f16
Mello <joaovdmello@gmail.com>
2026-04-27 12:04:24 -0300

    feat(deploy): adicionar vercel configs para deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 import { remarkReadingTime } from './src/utils/remark-reading-time.mjs';
 
 export default defineConfig({
   site: 'https://kernia.com.br/',
+  output: 'static',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   integrations: [mdx(), sitemap()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
